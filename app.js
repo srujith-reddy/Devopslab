5f93bb58c7c24bb3f10d6094de2dd25c60d9633d
Srujith-Reddy <srujithreddy801@gmail.com>
2023-03-10 09:53:17 +0530

    registraton form modified

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const sumbit=document.getElementById('submitbutton');
-const nameofuser=document.getElementById('takename');
-const emailofuser=document.getElementById('takeemail');
-const pswdofuser=document.getElementById('takepswd');
-// const fs=require('fs');
-sumbit.addEventListener('click',function(){
-            window.location.href='new.html'
-            let user_name;
-            user_name=nameofuser.value;
-            let user_email;
-            user_email=emailofuser.value;
-            let pswd;
-            pswed=pswdofuser.value;
-            console.log(user_email);
-            
-});
+This is a new branch
